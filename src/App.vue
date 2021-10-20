be2288e8276da2412c5e7fe1345a0b0c2b841838
lin557 <lin557@126.com>
2021-10-20 13:19:27 +0800

    add mp4 source

--- src/App.vue
+++ src/App.vue
@@ -153,10 +153,11 @@ export default {
         // src: '//81.71.36.80:8080/live/15981010700_3.flv',
         // src: '//81.71.36.80:9199/qinghuachi.flv',
         // src: '//81.71.36.80:9199/gbqq.mp4',
+        src: '//81.71.36.80:9199/oceans.mp4',
         // src: '//www.mvbox.net/mv/Z51-7QshVUM/%E6%88%B4%E7%BE%BD%E5%BD%A4%20-%20%E6%9D%A5%E8%BF%9F%20_%20d%C3%A0i%20y%C7%94%20t%C3%B3ng%20-%20l%C3%A1i%20ch%C3%AD%20_%20Coming%20Late%20-%20Dai%20Yutong%E3%80%8C%E6%88%91%E9%80%99%E4%B8%80%E6%AC%A1%E7%B5%82%E7%A9%B6%E9%82%84%E6%98%AF%E4%BE%86%E5%BE%97%E5%A4%AA%E9%81%B2%EF%BC%8C%E9%8C%AF%E9%81%8E%E4%BA%86%E4%BD%A0%EF%BC%8C%E6%AD%A4%E5%BE%8C%E6%89%80%E6%9C%89%E7%9A%84%E6%95%85%E4%BA%8B%E3%80%82%E3%80%8D%E2%99%AB%E5%8B%95%E6%85%8B%E6%AD%8C%E8%A9%9Elyrics%20%E2%99%AB.mp4',
         // src: 'http://221.237.209.57:9087/mdvr/live/15981010700_4.flv',
         //src: '//d2zihajmogu5jn.cloudfront.net/bipbop-advanced/gear3/prog_index.m3u8',
-        src: '//dno-xiu-hd.youku.com/rtp/stream_alias_1587073003_8131348.flv?auth_key=1634706297-0-0-e146eedf18592f9623bd33315557e083',
+        //src: '//dno-xiu-hd.youku.com/rtp/stream_alias_1587073003_8131348.flv?auth_key=1634706297-0-0-e146eedf18592f9623bd33315557e083',
         info: '粤B-8125L CH3',
         content: [
           {
